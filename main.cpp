215a39ec3829840d7c3103d9ec2955a008b3e650
alga <@>
2024-02-13 21:48:53 +0200

    Non recursive version of input_to_commands function

--- main.cpp
+++ main.cpp
@@ -56,18 +56,84 @@ std::map<std::string::size_type, std::string::size_type> find_matched_quotes(
     }
      if(is_quoted)
         {
-            quotes_positions.insert(std::make_pair(opening_quote, std::string::npos));
+            quotes_positions.insert(std::make_pair(opening_quote, 
+                        std::string::npos));
         }
     return quotes_positions;
 }
+// function to check if passed position inside one of the ranges provided
+// in map of indexes
+bool is_pos_quoted(const std::map<std::string::size_type, std::string::size_type> 
+        quotes_map, std::string::size_type pos)
+{
+    for(const std::pair<const std::string::size_type, std::string::size_type> 
+            &range_points : quotes_map)
+    {
+        if(range_points.first > pos)
+        {
+            return false;
+        }
+        // first property is the begining of the range, second is the end
+        if(range_points.first < pos && pos < range_points.second)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+// splits user input into the simplest sets of commands
+// !!!!!
+// Commands::Commands is replaced for debug
+// recursivly disassembles input, initializes new Command objects
+bool input_to_commands(
+        std::map<std::string::size_type, std::string::size_type> quotes_map,
+        std::string &src)
+{
+    std::string priority_commands;
+    std::string::size_type start_pos = 0;
+    std::string::size_type end_pos = 0;
 
-/*
-std::vector<std::string> split_row_input(std::string src)
+    while(start_pos != std::string::npos && start_pos <= src.length()-1)
+    {
+        start_pos = src.find('(', start_pos);        
+        // This segment looks for code code in (  )
+        if(!is_pos_quoted(quotes_map, start_pos))
+        {
+            end_pos = src.find(')', end_pos);
+            if(end_pos != std::string::npos && 
+                !is_pos_quoted(quotes_map, end_pos))
+            {
+                priority_commands = src.substr(start_pos, end_pos - start_pos);
+                std::string tmp_sliced_str = src.substr(0, start_pos);
+                if(end_pos != src.length() - 1)
+                {
+                    tmp_sliced_str += src.substr(end_pos + 1, src.length() - end_pos - 1);
+                }
+                // part of row input is deleted
+                src = tmp_sliced_str;
+            }
+        }
+        if(start_pos != std::string::npos && start_pos != src.length()-1)
+        {
+            start_pos++;
+        }
+        if(end_pos != std::string::npos && end_pos != src.length() - 1)
+        {
+            end_pos++;
+        }
+    }
+    return false;   
+}
+std::vector<std::string> split_row_input(std::string &src)
 {
     std::map<std::string::size_type, std::string::size_type> quotes_positions 
                 = find_matched_quotes(src);
+    input_to_commands(quotes_positions, src);
+    std::cout << src << std::endl;
+    std::vector<std::string> d;
+    return d;
 }
-*/
+
 int main()
 {
     std::string input;
@@ -87,13 +153,7 @@ int main()
 // Testing quotes
     while(input != "exit")
     {
-        std::map<std::string::size_type, std::string::size_type> quotes_positions
-            = find_matched_quotes(input);
-        for(const std::pair<const std::string::size_type, std::string::size_type>
-                &pos_pair : quotes_positions)
-        {
-            std::cout << pos_pair.first << " " << pos_pair.second << std::endl;
-        }
+        split_row_input(input);
         getline(std::cin, input);
     }
     return 0;
